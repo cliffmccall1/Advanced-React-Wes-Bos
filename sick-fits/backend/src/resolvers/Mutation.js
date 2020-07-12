d8f018d0873de534c6fb981b216d561282dfd503
Clifford McCall <cliff@Cliffords-MacBook-Pro.local>
2020-07-12 09:59:59 -0400

    Completed Module 4

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,8 +6,29 @@ const Mutations = {
             },
         }, info);
         return item;
-    }
+    },
+    updateItem(parent, args, ctx, info) {
+        const updates = { ...args };
+        // remove id from updates
+        delete updates.id;
+        // run update method
+        return ctx.db.mutaion.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info
+        );
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // find item
+        const item = await ctx.db.query.item({ where }, `{ id title}`);
+        // check if user has permissions 
 
+        // delete it
+        return ctx.db.mutation.deleteItem({ where }, info)
+    }
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/styles/Form.js
+++ sick-fits/frontend/components/styles/Form.js
@@ -3,12 +3,9 @@ import styled, { keyframes } from 'styled-components';
 const loading = keyframes`
   from {
     background-position: 0 0;
-    /* rotate: 0; */
   }
-
   to {
     background-position: 100% 100%;
-    /* rotate: 360deg; */
   }
 `;
 
@@ -49,7 +46,6 @@ const Form = styled.form`
   fieldset {
     border: 0;
     padding: 0;
-
     &[disabled] {
       opacity: 0.5;
     }
