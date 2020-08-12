e86e427946d1b79d4873f97a9b3ba84252052980
Clifford McCall <cliff@Cliffords-MacBook-Pro.local>
2020-08-11 21:41:19 -0400

    V5.29 Backend Password Reset Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,76 +1,132 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args
-            },
-        }, info);
-        return item;
-    },
-    updateItem(parent, args, ctx, info) {
-        const updates = { ...args };
-        // remove id from updates
-        delete updates.id;
-        // run update method
-        return ctx.db.mutaion.updateItem({
-            data: updates,
-            where: {
-                id: args.id
-            }
-        }, info
-        );
-    },
-    async deleteItem(parent, args, ctx, info) {
-        const where = { id: args.id };
-        // find item
-        const item = await ctx.db.query.item({ where }, `{ id title}`);
-        // check if user has permissions 
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    // remove id from updates
+    delete updates.id;
+    // run update method
+    return ctx.db.mutaion.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // check if user has permissions
 
-        // delete it
-        return ctx.db.mutation.deleteItem({ where }, info)
-    },
-    async signup(parent, args, ctx, info) {
-        args.email = args.email.toLowerCase();
-        const password = await bcrypt.hash(args.password, 10);
-        const user = await ctx.db.mutation.createUser(
-            {
-                data: {
-                    ...args,
-                    password,
-                    permissions: { set: ['USER'] },
-                },
-            }, info
-        );
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365
-        });
-        return user;
-    },
-    async signin(parent, { email, password }, ctx, info) {
-        const user = await ctx.db.query.user({where: { email }});
-        if(!user) {
-            throw new Error(`No such user found for email ${email}`);
-        }
-        const valid = await bcrypt.compare(password, user.password);
-        if(!valid) {
-            throw new Error('Invalid Password');
-        }
-        const token = jut.sign({ userId: user.id }, process.env.APP_SECRET);
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365
-        });
-        return user;
-    },
-    signout(parent, args, ctx, info) {
-        ctx.response.clearCookie('token');
-        return { message: 'Goodbye!' };
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] },
+        },
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
     }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password");
+    }
+    const token = jut.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye!" };
+  },
+  async requestReset(parent, args, ctx, info) {
+    const randomBytesPromiseified = promisify(randomBytes);
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${args.email}`);
+    }
+    const resetToken = (await randomBytesPromiseified(20)).toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000;
+    const res = ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // check if passwords match
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Passwords don't match");
+    }
+    // check if its expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    });
+    if (!user) {
+      throw new Error("This token is either invalid or expired");
+    }
+    // hash new password
+    const password = await bcrypt.hash(args.password, 10);
+    // save new password to user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    // generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    // set the JWT cookie
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    return updatedUser;
+  },
 };
 
 module.exports = Mutations;
